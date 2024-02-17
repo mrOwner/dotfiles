053c0a5d80827b36ba9832de62898fec8a76a0e0
mrOwner <starceff@gmail.com>
2024-02-17 20:07:52 +0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dotfiles
+
+```bash
+git clone git@github.com:mrOwner/dotfiles.git ~/.dotfiles
+sudo pacman -S stow
+stow --adopt fish # or other name
+```
+
---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,32 @@
+set -g fish_greeting
+
+if status is-interactive
+    starship init fish | source
+end
+
+# List Directory
+alias ls="lsd"
+alias l="ls -lt --date relative"
+alias la="ls -a --date date"
+alias ll="ls -l --date date"
+alias lla="ls -la --date date"
+alias lt="ls --tree"
+
+
+alias cat="bat"
+
+alias v="nvim"
+alias vim="nvim"
+
+# Handy change dir shortcuts
+abbr .. 'cd ..'
+abbr ... 'cd ../..'
+abbr .3 'cd ../../..'
+abbr .4 'cd ../../../..'
+abbr .5 'cd ../../../../..'
+
+# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
+abbr mkdir 'mkdir -p'
+
+# Fixes "Error opening terminal: xterm-kitty" when using the default kitty term to open some programs through ssh
+alias ssh='kitten ssh'
