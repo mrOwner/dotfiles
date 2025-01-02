ebbca5a6dc4fb12f93ce21c973ee87fe7bb8edaf
mrOwner <starceff@gmail.com>
2025-01-02 12:21:00 +0400

    update nvim

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -30,7 +30,7 @@ require("lazy").setup({
       { import = "lazyvim.plugins.extras.lang.typescript" },
       -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
       { import = "lazyvim.plugins.extras.coding.luasnip" },
-      { import = "lazyvim.plugins.extras.coding.codeium" },
+      { import = "lazyvim.plugins.extras.ai.codeium" },
       { import = "lazyvim.plugins.extras.coding.yanky" },
       { import = "lazyvim.plugins.extras.formatting.prettier" },
       { import = "lazyvim.plugins.extras.coding.mini-surround" },
